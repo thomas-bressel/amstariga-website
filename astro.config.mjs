bb4bf830da58c9360dd3c16f7ad11bb90c5018ab
thomas-bressel <tbressel@outlook.fr>
2026-04-14 00:21:07 +0200

    core: initializing a new projet with astro incliding react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [react()]
+});
