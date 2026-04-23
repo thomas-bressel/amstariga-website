4a086420fa288d2cdc0a81e190b6f50028de4026
thomas-bressel <tbressel@outlook.fr>
2026-04-23 14:08:32 +0200

    fix: change maintenace conditionnal

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { defineMiddleware } from 'astro:middleware';
 
 export const onRequest = defineMiddleware((context, next) => {
-    const maintenance = import.meta.env.MAINTENANCE === 'true';
+    const maintenance = (import.meta.env.MAINTENANCE ?? process.env.MAINTENANCE) === 'true';
     const path = new URL(context.request.url).pathname;
 
     if (maintenance && path !== '/maintenance') {
