a98ef8e1bccd1aba5bc64ddfd47c1e2f07af0ec1
thomas-bressel <tbressel@outlook.fr>
2026-04-23 13:47:58 +0200

    update: add a maintenance page

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { defineMiddleware } from 'astro:middleware';
+
+export const onRequest = defineMiddleware((context, next) => {
+    const maintenance = import.meta.env.MAINTENANCE === 'true';
+    const path = new URL(context.request.url).pathname;
+
+    if (maintenance && path !== '/maintenance') {
+        return context.redirect('/maintenance', 302);
+    }
+
+    return next();
+});
